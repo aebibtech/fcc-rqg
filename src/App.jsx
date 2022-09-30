53b97ab5a153be22d6e37380b05795da8c899329
aebibtech <paul.camano@gmail.com>
2022-09-30 16:08:08 +0800

    Get random quote on first load properly work

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,24 @@ import QuoteBox from './QuoteBox'
 
 function App() {
   const [quotesData, setQuotesData] = useState({})
-  const [currQuote, setCurrQuote] = useState({ quote: "You can’t use up creativity. The more you use, the more you have.", author: "Maya Angelou"})
+  const [currQuote, setCurrQuote] = useState({})
 
   useEffect(() => {
     axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
           .then(res => {
-            setQuotesData(res.data)
+            setQuotesData(res.data.quotes)
+            setCurrQuote(res.data.quotes[Math.floor(Math.random() * res.data.quotes.length)])
           }).catch(e => {
-            console.log(e.message)
+            setCurrQuote({ quote: "Failed to get data from server.", author: ""})
           })
   }, [])
 
   function getRandomQuote() {
-    let qts = quotesData.quotes
-    return qts[Math.floor(Math.random() * qts.length)]
+    return quotesData[Math.floor(Math.random() * quotesData.length)]
   }
 
   function newQuote() {
-    setCurrQuote(() => getRandomQuote())
+      setCurrQuote(() => getRandomQuote())
   }
   return (
     <>
