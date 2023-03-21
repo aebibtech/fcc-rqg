950136c76545d141ffb0600743c5c0df5f86246a
aebibtech <paul.camano@gmail.com>
2023-03-21 20:34:56 +0800

    Added proper favicon

--- src/App.jsx
+++ src/App.jsx
@@ -3,62 +3,62 @@ import axios from 'axios'
 import QuoteBox from './QuoteBox'
 
 function App() {
-  const [quotesData, setQuotesData] = useState({})
-  const [currQuote, setCurrQuote] = useState({})
-  const [color, setColor] = useState({})
-  const animation = { transition: "all 2s ease", WebkitTransition: "all 2s ease", MozTransition: "all 2s ease" }
+	const [quotesData, setQuotesData] = useState({})
+	const [currQuote, setCurrQuote] = useState({})
+	const [color, setColor] = useState({})
+	const animation = { transition: "all 2s ease", WebkitTransition: "all 2s ease", MozTransition: "all 2s ease" }
 
-  const colors = [
-    '#16a085', '#27ae60', '#2c3e50',
-    '#f39c12', '#e74c3c', '#9b59b6',
-    '#FB6964', '#342224', '#472E32',
-    '#BDBB99', '#77B1A9', '#73A857'
-  ];
+	const colors = [
+		'#16a085', '#27ae60', '#2c3e50',
+		'#f39c12', '#e74c3c', '#9b59b6',
+		'#FB6964', '#342224', '#472E32',
+		'#BDBB99', '#77B1A9', '#73A857'
+	];
 
-  useEffect(() => {
-    async function getData(){
-      try{
-        const res = await axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
-        setQuotesData(res.data.quotes)
-        setCurrQuote(res.data.quotes[Math.floor(Math.random() * res.data.quotes.length)])
-        setColor({
-          color: colors[Math.floor(Math.random() * colors.length)],
-          ...animation
-        })
-      }
-      catch(err){
-        setCurrQuote({ quote: "Failed to get data from server.", author: ""})
-      }
-    }
-    getData()
-  }, [])
+	useEffect(() => {
+		async function getData(){
+		try{
+			const res = await axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+			setQuotesData(res.data.quotes)
+			setCurrQuote(res.data.quotes[Math.floor(Math.random() * res.data.quotes.length)])
+			setColor({
+			color: colors[Math.floor(Math.random() * colors.length)],
+			...animation
+			})
+		}
+		catch(err){
+			setCurrQuote({ quote: "Failed to get data from server.", author: ""})
+		}
+		}
+		getData()
+	}, [])
 
-  function getRandomQuote() {
-    return quotesData[Math.floor(Math.random() * quotesData.length)]
-  }
+	function getRandomQuote() {
+		return quotesData[Math.floor(Math.random() * quotesData.length)]
+	}
 
-  function newQuote() {
-      setCurrQuote(() => getRandomQuote())
-      getRandomColor()
-  }
+	function newQuote() {
+		setCurrQuote(() => getRandomQuote())
+		getRandomColor()
+	}
 
-  function getRandomColor() {
-    setColor(() => {
-      let randomColor = colors[Math.floor(Math.random() * colors.length)]
-      return { color: randomColor, ...animation }})
-  }
-  return (
-    <div style={ { backgroundColor: color.color, ...animation } }>
-    <div className="jumbotron-fluid container-fluid text-center">
-      <h1 className="display-4 fw-bold">Random Quote Generator</h1>
-      <p className="lead">Share some quotes to your friends!</p>
-    </div>
-    <QuoteBox quote={currQuote} handler={newQuote} styleObj={color} />
-    <div className="container-fluid text-center footer">
-      <span>&copy;</span> <strong><a href="https://aebibtech.com" target="_blank" rel="noreferrer noopener">Aebibtech</a></strong>
-    </div>
-    </div>
-  )
+	function getRandomColor() {
+		setColor(() => {
+		let randomColor = colors[Math.floor(Math.random() * colors.length)]
+		return { color: randomColor, ...animation }})
+	}
+	return (
+		<div style={ { backgroundColor: color.color, ...animation } }>
+		<div className="jumbotron-fluid container-fluid text-center">
+		<h1 className="display-4 fw-bold">Random Quote Generator</h1>
+		<p className="lead">Share some quotes to your friends!</p>
+		</div>
+		<QuoteBox quote={currQuote} handler={newQuote} styleObj={color} />
+		<div className="container-fluid text-center footer">
+		<span>&copy;</span> <strong><a href="https://aebibtech.com" target="_blank" rel="noreferrer noopener">Aebibtech</a></strong>
+		</div>
+		</div>
+	)
 }
 
 export default App
--- src/QuoteBox.jsx
+++ src/QuoteBox.jsx
@@ -6,22 +6,22 @@ function QuoteBox({ quote, handler, styleObj }) {
     const btnStyle = { backgroundColor: styleObj.color, transition: "all 2s ease", WebkitTransition: "all 2s ease", MozTransition: "all 2s ease" }
     return (
         <div style={styleObj} id="main">
-        <div className="container-fluid" id="quote-box">
-            <div className="d-flex justify-content-start">
-                <div className="col col-xs-4"><h3 className="text-center"><i className='fa fa-quote-left'></i> <span id="text">{qt.quote}</span></h3></div>
-            </div>
-            <div className="d-flex justify-content-end" id="author">
-                <p className="lead">- {qt.author}</p>
-            </div>
-            <div className="d-flex justify-content-between" id="buttons">
-                <div>
-                    <strong>Share on:</strong> <a style={btnStyle} className='btn btn-dark' id="tweet-quote" href={twitterSharer.concat(`${qt.quote} ${qt.author}`)} target="_blank" rel="noopener noreferrer"><i className='fa fa-twitter'></i></a>
+            <div className="container-fluid" id="quote-box">
+                <div className="d-flex justify-content-start">
+                    <div className="col col-xs-4"><h3 className="text-center"><i className='fa fa-quote-left'></i> <span id="text">{qt.quote}</span></h3></div>
                 </div>
-                <div>
-                    <button style={btnStyle} className='btn btn-dark' id="new-quote" onClick={handler} disabled={qt.author === ""}>New quote</button>
+                <div className="d-flex justify-content-end" id="author">
+                    <p className="lead">- {qt.author}</p>
+                </div>
+                <div className="d-flex justify-content-between" id="buttons">
+                    <div>
+                        <strong>Share on:</strong> <a style={btnStyle} className='btn btn-dark' id="tweet-quote" href={twitterSharer.concat(`${qt.quote} ${qt.author}`)} target="_blank" rel="noopener noreferrer"><i className='fa fa-twitter'></i></a>
+                    </div>
+                    <div>
+                        <button style={btnStyle} className='btn btn-dark' id="new-quote" onClick={handler} disabled={qt.author === ""}>New quote</button>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
