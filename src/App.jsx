d577c688a619a31c5203ad22c50a84591fa4d200
aebibtech <paul.camano@gmail.com>
2023-03-21 20:40:31 +0800

    Fix indentation App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -49,14 +49,14 @@ function App() {
 	}
 	return (
 		<div style={ { backgroundColor: color.color, ...animation } }>
-		<div className="jumbotron-fluid container-fluid text-center">
-		<h1 className="display-4 fw-bold">Random Quote Generator</h1>
-		<p className="lead">Share some quotes to your friends!</p>
-		</div>
-		<QuoteBox quote={currQuote} handler={newQuote} styleObj={color} />
-		<div className="container-fluid text-center footer">
-		<span>&copy;</span> <strong><a href="https://aebibtech.com" target="_blank" rel="noreferrer noopener">Aebibtech</a></strong>
-		</div>
+			<div className="jumbotron-fluid container-fluid text-center">
+				<h1 className="display-4 fw-bold">Random Quote Generator</h1>
+				<p className="lead">Share some quotes to your friends!</p>
+			</div>
+			<QuoteBox quote={currQuote} handler={newQuote} styleObj={color} />
+			<div className="container-fluid text-center footer">
+				<span>&copy;</span> <strong><a href="https://aebibtech.com" target="_blank" rel="noreferrer noopener">Aebibtech</a></strong>
+			</div>
 		</div>
 	)
 }
