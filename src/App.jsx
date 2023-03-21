09f9a5a48d2ed3eac5ee969cbbd3573756671cd3
aebibtech <paul.camano@gmail.com>
2023-03-21 20:23:45 +0800

    Convert promise to async/await

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [color, setColor] = useState({})
   const animation = { transition: "all 2s ease", WebkitTransition: "all 2s ease", MozTransition: "all 2s ease" }
 
-  let colors = [
+  const colors = [
     '#16a085', '#27ae60', '#2c3e50',
     '#f39c12', '#e74c3c', '#9b59b6',
     '#FB6964', '#342224', '#472E32',
@@ -16,14 +16,21 @@ function App() {
   ];
 
   useEffect(() => {
-    axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
-          .then(res => {
-            setQuotesData(res.data.quotes)
-            setCurrQuote(res.data.quotes[Math.floor(Math.random() * res.data.quotes.length)])
-            setColor({ color: colors[Math.floor(Math.random() * colors.length)], ...animation })
-          }).catch(e => {
-            setCurrQuote({ quote: "Failed to get data from server.", author: ""})
-          })
+    async function getData(){
+      try{
+        const res = await axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+        setQuotesData(res.data.quotes)
+        setCurrQuote(res.data.quotes[Math.floor(Math.random() * res.data.quotes.length)])
+        setColor({
+          color: colors[Math.floor(Math.random() * colors.length)],
+          ...animation
+        })
+      }
+      catch(err){
+        setCurrQuote({ quote: "Failed to get data from server.", author: ""})
+      }
+    }
+    getData()
   }, [])
 
   function getRandomQuote() {
@@ -43,12 +50,12 @@ function App() {
   return (
     <div style={ { backgroundColor: color.color, ...animation } }>
     <div className="jumbotron-fluid container-fluid text-center">
-      <h1 className="display-4">Random Quote Generator</h1>
+      <h1 className="display-4 fw-bold">Random Quote Generator</h1>
       <p className="lead">Share some quotes to your friends!</p>
     </div>
     <QuoteBox quote={currQuote} handler={newQuote} styleObj={color} />
     <div className="container-fluid text-center footer">
-      <strong><a href="https://aebibtech.com" target="_blank" rel="noreferrer noopener">by Aebibtech</a></strong>
+      <span>&copy;</span> <strong><a href="https://aebibtech.com" target="_blank" rel="noreferrer noopener">Aebibtech</a></strong>
     </div>
     </div>
   )
