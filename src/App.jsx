def176024950abf0c7cb954d5339df1754b08a91
aebibtech <paul.camano@gmail.com>
2023-04-24 23:21:39 +0800

    Refactor random colors generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import QuoteBox from './QuoteBox'
+import Styles from '../styles.css'
 
 function App() {
 	const [quotesData, setQuotesData] = useState({})
@@ -8,29 +9,22 @@ function App() {
 	const [color, setColor] = useState({})
 	const animation = { transition: "all 2s ease", WebkitTransition: "all 2s ease", MozTransition: "all 2s ease" }
 
-	const colors = [
-		'#16a085', '#27ae60', '#2c3e50',
-		'#f39c12', '#e74c3c', '#9b59b6',
-		'#FB6964', '#342224', '#472E32',
-		'#BDBB99', '#77B1A9', '#73A857'
-	];
+	function getNumber(){
+		const DARK_VALUES = "01234567"
+		return DARK_VALUES[Math.floor(Math.random() * DARK_VALUES.length)]
+	}
 
-	useEffect(function(){
-		(async function(){
-			try{
-				const res = await axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
-				setQuotesData(res.data.quotes)
-				setCurrQuote(res.data.quotes[Math.floor(Math.random() * res.data.quotes.length)])
-				setColor({
-					color: colors[Math.floor(Math.random() * colors.length)],
-					...animation
-				})
-			}
-			catch(err){
-				setCurrQuote({ quote: "Failed to get data from server.", author: ""})
-			}
-		})()
-	}, [])
+	function getRandomColor() {
+		let randomColor = "#"
+		for(let i = 0; i < 3; i++){
+			randomColor += getNumber()
+		}
+		return { color: randomColor, ...animation }
+	}
+
+	function setBodyColor(color){
+		document.body.style.setProperty("--bgc", color)
+	}
 
 	function getRandomQuote() {
 		return quotesData[Math.floor(Math.random() * quotesData.length)]
@@ -38,17 +32,29 @@ function App() {
 
 	function newQuote() {
 		setCurrQuote(() => getRandomQuote())
-		getRandomColor()
+		setColor(() => getRandomColor())
 	}
 
-	function getRandomColor() {
-		setColor(() => {
-			let randomColor = colors[Math.floor(Math.random() * colors.length)]
-			return { color: randomColor, ...animation }
-		})
-	}
+	useEffect(function(){
+		(async function(){
+			try{
+				const res = await axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+				setQuotesData(res.data.quotes)
+				setCurrQuote(res.data.quotes[Math.floor(Math.random() * res.data.quotes.length)])
+				setColor(getRandomColor())
+			}
+			catch(err){
+				setCurrQuote({ quote: "Failed to get data from server.", author: ""})
+			}
+		})()
+	}, [])
+
+	useEffect(function(){
+		setBodyColor(color.color)
+	}, [color])
+
 	return (
-		<div style={ { backgroundColor: color.color, ...animation } }>
+		<div style={animation}>
 			<div className="jumbotron-fluid container-fluid text-center">
 				<h1 className="display-4 fw-bold">Random Quote Generator</h1>
 				<p className="lead">Share some quotes to your friends!</p>
